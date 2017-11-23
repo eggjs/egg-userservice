f19322fc7c5546e8630a0a81f73ca9d09dc50cdd
Yiyu He <dead_horse@qq.com>
2017-11-23 15:04:39 +0800

    refactor: use async function and support egg@2 (#8)

--- test/userservice.test.js
+++ test/userservice.test.js
@@ -1,37 +1,70 @@
 'use strict';
 
-const request = require('supertest');
 const mock = require('egg-mock');
 
 describe('test/userservice.test.js', () => {
   let app;
-  before(done => {
-    app = mock.app({
-      baseDir: 'apps/userservice',
+  describe('generator', () => {
+    before(done => {
+      app = mock.app({
+        baseDir: 'apps/userservice',
+      });
+      app.ready(done);
     });
-    app.ready(done);
+
+    it('should get user and userId', done => {
+      app.httpRequest()
+        .get('/?uid=456&name=shaoshuai0102')
+        .expect({
+          userId: '456',
+          user: {
+            uid: '456',
+            name: 'shaoshuai0102',
+          },
+        })
+        .expect(200, done);
+    });
+
+    it('should return empty', done => {
+      app.httpRequest()
+        .get('/')
+        .expect({
+          userId: null,
+          user: null,
+        })
+        .expect(200, done);
+    });
   });
 
-  it('should get user and userId', done => {
-    request(app.callback())
-    .get('/?uid=456&name=shaoshuai0102')
-    .expect({
-      userId: '456',
-      user: {
-        uid: '456',
-        name: 'shaoshuai0102',
-      },
-    })
-    .expect(200, done);
-  });
+  describe('generator', () => {
+    before(done => {
+      app = mock.app({
+        baseDir: 'apps/userservice-async',
+      });
+      app.ready(done);
+    });
 
-  it('should return empty', done => {
-    request(app.callback())
-    .get('/')
-    .expect({
-      userId: null,
-      user: null,
-    })
-    .expect(200, done);
+    it('should get user and userId', done => {
+      app.httpRequest()
+        .get('/?uid=456&name=shaoshuai0102')
+        .expect({
+          userId: '456',
+          user: {
+            uid: '456',
+            name: 'shaoshuai0102',
+          },
+        })
+        .expect(200, done);
+    });
+
+    it('should return empty', done => {
+      app.httpRequest()
+        .get('/')
+        .expect({
+          userId: null,
+          user: null,
+        })
+        .expect(200, done);
+    });
   });
 });
